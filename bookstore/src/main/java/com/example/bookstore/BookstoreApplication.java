7f663d3eb9d81d28ec73cd3368cba3a1336ba4de
vikas <vikas.kumar@suhora.com>
2025-10-30 18:08:36 +0530

    nothgin

---
+++ bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -0,0 +1,14 @@
+package com.example.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+        System.out.println("Server started at http://localhost:1123");
+	}
+
+}
